006c216be0de0781592e2684d0829a317369923b
App Academy Student <student@appacademy.io>
2015-04-30 12:27:06 -0700

    Convert coordinates from user's point of view to program's point of view

--- board.rb
+++ board.rb
@@ -1,16 +1,44 @@
 # board
 
+class Board
+  attr_accessor :grid
 
 
-def occupied_at?(pos)
 
+  def initialize
+    self.grid = Array.new(8){Array.new(8){}}
+  end
 
-end
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+  def []=(pos, arg)
+    x, y = pos
+    grid[x][y]
+  end
 
-def piece_at(pos)
-end
+  def occupied_at?(pos)
+  end
 
-def piece_on_board?(pos)
-  x_pos, y_pos = pos
-  x_pos < board.grid.size && x >= 0 && y_pos < board.grid.size && y_pos >= 0
+  def piece_at(pos)
+  end
+
+  def piece_on_board?(pos)
+    x_pos, y_pos = pos
+    x_pos < board.grid.size && x >= 0 && y_pos < board.grid.size && y_pos >= 0
+  end
+
+  def set_up_board
+
+  end
+
+  def convert_coord_for_ui_perspective(coor)
+    # program_x + grid.size - (program_x + (program_x + 1))
+    # (grid.size - 1 - program_x) <= to get from program_x to x_ui
+    ui_x, ui_y = coor
+    program_x = grid.size - ui_x - 1
+    program_y = ui_x
+    [program_x, program_y]
+  end
 end
